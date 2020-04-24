20fc5858fa8678c337e5b3745b96205629572cb5
Victor Horvat <u6950099@anu.edu.au>
2020-04-24 11:29:47 +1000

    Some functions have been renamed oneCellCheck has been added as a helper function to detect   rows, columns or 3x3's with only one missing space
    emptyCellSearch has been added to go through the board and fill out
      rows or columns with only one empty cell
    
    rowToCol has been added as a helper function to convert the board of rows
      to a board of columns for emptyCellSearch

--- .idea/Main.py
+++ .idea/Main.py
@@ -12,11 +12,11 @@ testFullBoard =[
     [3,7,4,9,8,2,5,6,1]
     ]
 testBoard =[
-    [5,3,1,6,2,7,9,8,4],
+    [" ",3,1,6,2,7,9,8,4],
     [6,4,9,8,1,3,2,5,7],
     [8,2,7,5,9,4,6,1,3],
     [4,9,6,1,7,5,8,3,2],
-    [2,1,8,3," ",9,7,4,5],
+    [2,1,8,3,6,9,7,4,5],
     [7,5,3,2,4,8,1,9,6],
     [9,6,2,4,5,1,3,7,8],
     [1,8,5,7,3,6,4,2,9],
@@ -26,18 +26,29 @@ testBoard =[
 numberList = [1,2,3,4,5,6,7,8,9]
 
 
+
 # Combs through a row to find and fill an empty cell
-def oneCellRowSearch(row):
+def oneCellFill(row):
     for cell in row:
         if cell == " ":
-            penFill = missingCells(row)
+            penFill = cellFillHelper(row)
             row[row.index(" ")] = penFill[0]
 
+# Checks to see if there is only one empty cell in a row,column or 3x3
+def oneCellCheck(row):
+    emptyCount = 0
+    for number in row:
+        if number == " ":
+            emptyCount = emptyCount + 1
+    if emptyCount == 1:
+        return True
+    else:
+        return False
 
 # Iterates through numbers 1-9 and returns the missing values for empty cells
-def missingCells(row):
+def cellFillHelper(row):
     # This lets numberList remain for future checks
-    tempNumberList = numberList
+    tempNumberList = [1,2,3,4,5,6,7,8,9]
     for cell in row:
         for number in tempNumberList:
             if number == cell:
@@ -45,4 +56,34 @@ def missingCells(row):
     return(tempNumberList)
 
 
-oneCellRowSearch([2,1,8,3," ",9,7,4,5])
+# # Searches the sudoku for missing cells
+def emptyCellSearch(board):
+    # Check rows
+    for row in board:
+        if oneCellCheck(row):
+            oneCellFill(row)
+    # Check columns
+    colBoard = rowToCol(board)
+    for col in colBoard:
+        if oneCellCheck(col):
+            oneCellFill(col)
+
+
+# Converts from board of rows, to board of columns
+def rowToCol(board):
+    print(board)
+    colBoard = []
+    column = []
+    for x in range((len(board[0]))):
+        for y in board:
+            # print("y: "+str(y))
+            # print("x: "+str(x))
+            column.append(y[x])
+        colBoard.append(column)
+        column = []
+    print(colBoard)
+
+
+# emptyCellSearch(testBoard)
+
+rowToCol(testFullBoard)
