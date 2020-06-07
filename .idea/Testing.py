f54a2c54569e17c8449edf0ef7b365b3b2020033
Victor Horvat <61690631+vichorvat@users.noreply.github.com>
2020-06-07 20:23:35 +1000

    Blackbox testing set up for stringToBoard function

--- .idea/Testing.py
+++ .idea/Testing.py
@@ -1,4 +1,4 @@
-
+import unittest
 
 def SameCheck(fullBoard, emptyBoard):
     for i in range(0,9):
@@ -6,3 +6,8 @@ def SameCheck(fullBoard, emptyBoard):
             if fullBoard[i][j].value != emptyBoard[i][j].value:
                 return False
     return True
+
+
+# Black box testing for GUI.stringToBoard
+print("GUI.stringToBoard test == " + SameCheck(GUI.stringToBoard("568742913197368254342591687685213479734859162219476538473625891851934726926187345"),
+    Solver.testFullBoard3))
